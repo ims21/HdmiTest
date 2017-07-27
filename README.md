260f1b51d5f4382307e4c5907fc540c994e42ee1
ims <ims21@users.sourceforge.net>
2017-07-27 16:09:06 +0200

    First push my plugin to github

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HdmiTest
+
+- plugin for testing HDMI-CEC
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+import setup_translate
+
+pkg = 'Extensions.HdmiTest'
+setup (name = 'enigma2-plugin-extensions-hdmitest',
+       version = '0.40',
+       description = 'plugin for testing HDMI-CEC',
+       packages = [pkg],
+       package_dir = {pkg: 'plugin'},
+       package_data = {pkg: ['locale/*.pot', 'locale/*/LC_MESSAGES/*.mo']},
+       cmdclass = setup_translate.cmdclass, # for translation
+      )
